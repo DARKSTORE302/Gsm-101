bdd0e744fc28896a82b34b17f32872ea4bbf4670
DARKSTORE302 <123235871+DARKSTORE302@users.noreply.github.com>
2023-02-06 15:49:17 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,32 +2,32 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
+global.botname = "GSM-MD BOT" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
 global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
+global.packname = "TEAM DARK ®"  
+global.myweb = "https://github.com/DARKSTORE302" // You can put any link 
 global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.headerText = "GSM BOT" // text for header of menu         
+global.DocumentTitle = "TEAMxDARK" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ownername = "GSM ATIQUE"   // Owner name //        
+global.ownernumber = ['923432680285'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : GSM_ATIQUE" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: TEAM DARK OFFICIAL"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.region = "Pakistan" // your country name
+global.state = "Punjab" // your state name
+global.district = "Sargodha" // your district name
+global.timezone = "Asia/Islamabad" // search on google if you don't know the timezone of your country //
+global.instagramId = "GSM_ATIQUE" // Your insta ID
+global.email = "gsmtechnicalyt@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['923432******','923460******']  // available soon
+global.sudo = ['923060******','923137******']  // Co - Owners //
 
 global.prefa = ['-','🥵','🔥']
 
